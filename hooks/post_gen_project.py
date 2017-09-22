5e5b45aead9746ef5306ed1e6f9801fd74e6e342
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-09-22 10:45:44 -0700

    Fixed hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,12 +11,12 @@ def remove_file(filepath):
 
 if __name__ == '__main__':
     if '{{cookiecutter.chef_template}}' != 'Sushi Chef':
-        remove_file('{}{}{}'.format('{{cookiecutter.project_slug}}', os.path.sep, 'sushichef.py'))
+        remove_file('sushichef.py')
         remove_file('examples{}{}'.format(os.path.sep, 'openstax_sushichef.py'))
         remove_file('examples{}{}'.format(os.path.sep, 'wikipedia_sushichef.py'))
 
     if '{{cookiecutter.chef_template}}' != 'Sous Chef':
-        remove_file('{}{}{}'.format('{{cookiecutter.project_slug}}', os.path.sep, 'souschef.py'))
+        remove_file('souschef.py')
         remove_file('utils{}{}'.format(os.path.sep, 'data_writer.py'))
         remove_file('utils{}{}'.format(os.path.sep, 'path_builder.py'))
         remove_file('examples{}{}'.format(os.path.sep, 'openstax_souschef.py'))
